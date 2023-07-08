7253e3cf27f7de303931546e98e705918e180970
Hamza Wattoo <hamzawattoo4321@gmail.com>
2023-07-08 15:44:16 +0500

    responsive issue fixed

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Headline = ({ text }) => {
-  return <h1 className="text-black text-center text-[30px] font-bold">{text}</h1>;
+  return <h1 className="text-black text-left sm:text-center text-[30px] font-bold">{text}</h1>;
 };
 
 export default Headline;
